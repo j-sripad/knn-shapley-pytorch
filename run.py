79ad250e1de5a3c95d4dd30b846c7d6684a56c8a
Abhiroop Tejomay <abhirooptejomay@gmail.com>
2021-05-15 15:31:28 +0000

    added run

---
+++ run.py
@@ -0,0 +1,20 @@
+from models.resnet import ResNet 
+from shapley.dshap import DShap 
+from utils.dataset import FashionMNISTDataModule
+from shapley.knn_shapley import KNNShapley
+
+def main():
+    dm = FashionMNISTDataModule() 
+    dm.setup() 
+
+    model = ResNet()
+
+    measure = KNNShapley()
+
+    dshap = DShap(dm, model, measure=measure)
+    scores = dshap.run() 
+    return scores
+
+if __name__ == "__main__":
+    scores = main()
+    print(scores)

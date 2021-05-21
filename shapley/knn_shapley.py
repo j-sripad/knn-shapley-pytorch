5baa988d479c4f449c34ad878aae4ffdc17e5ac4
Abhiroop Tejomay <abhirooptejomay@gmail.com>
2021-05-21 11:00:44 +0000

    calculate distances batch-wise

--- shapley/knn_shapley.py
+++ shapley/knn_shapley.py
@@ -29,22 +29,19 @@ class KNNShapley:
             y_train = torch.zeros(y_shape)
 
         stored = False
-        for i, (x, y) in enumerate(tqdm(dm.test_set)):
-            diff = None
+        for i, (x, y) in enumerate(tqdm(dm.test_set, desc="Computing Shapley")):
+            dist = torch.zeros(0)
             X = model(x.unsqueeze(0))
 
-            for j, (X_b, y_b) in enumerate(tqdm(dm.train_dataloader(), desc="Train Inference")):
+            for j, (X_b, y_b) in enumerate(dm.train_dataloader()):
                 X_hat = model(X_b) 
-                diff_b = (X_hat - X).reshape((X_hat.shape[0], -1))
-                
-                if diff is None:
-                    diff = diff_b 
-                else:
-                    diff = torch.cat([diff, diff_b])
+                diff = (X_hat - X).reshape((X_b.shape[0], -1))
+                dist_b = torch.einsum('ij, ij->i', diff, diff)
+                dist = torch.cat([dist, dist_b.detach()])
+
                 if not stored:
                     y_train = torch.cat([y_train, y_b])
 
-            dist = torch.einsum('ij, ij->i', diff, diff)
             idx = torch.argsort(dist)
             ans = y_train[idx]
             s[idx[N - 1]][i] = float(ans[N - 1] == y) / N
